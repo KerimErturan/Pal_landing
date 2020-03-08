4afc026ff53c144d651110949bc535167435272f
muzakparov <muzakparov@gmail.com>
2020-03-08 15:23:03 +0600

    init

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["es2015"] }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: {
+      index: "dist/index.html"
+    }
+  }
+};
